98a0d9af60339a63ead5c52588933e752fbc36a1
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-06-27 13:45:53 -0700

    add mimimize to gulp tasks

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ testing gulp tasks and deploy to gh-pages
 *npm install gulp --save --only=dev
 *npm install browserify --save --only=dev
 *npm install vinyl-source-stream --save --only=dev
-**setup browserify gupltask
+**setup browserify gulptask
+*npm install gulp-concat --save --only=dev
+**setup concat gulptask, make it a dependency for browserify
+*npm install gulp-uglify --save --only=dev
+**setup mimimize gulptask, make it a dependency for concat
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
+var mimimize = require('gulp-uglify');
 var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-
 
 gulp.task('jsConcat', function() {
   return gulp.src(['./src/js/form.js' ,'./src/js/ui.js'])
@@ -14,5 +14,11 @@ gulp.task('jsBrowserify', ['jsConcat'], function() {
   return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('jsMin', ['jsBrowserify'], function() {
+  return gulp.src('./dist/js/app.js')
+    .pipe(mimimize())
     .pipe(gulp.dest('./dist/js'));
 });
